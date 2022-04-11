4b35fb2aa927643e08a319ba6a00718963972020
Sham Targe <shamtarge@gmail.com>
2022-04-11 09:52:50 +0000

    Starting with shmat

--- shm.h
+++ shm.h
@@ -5,6 +5,6 @@
 //todo: check if can define structures for shared memory pages and regions here, and redeclare shared memory system call prototypes 
 void shared_memory_init(void);
 int shmget_util(int, int, int); //key, size, shmflag
-//int shmat(int, void*, int); //shmid, shmaddr, shmflag
+int shmat(int, void*, int); //shmid, shmaddr, shmflag
 //int shmdet(void*); //shmaddr
 //int shmctl(int, int, struct shmid_ds*); //shmid, cmd, struct shmid_ds* buf
